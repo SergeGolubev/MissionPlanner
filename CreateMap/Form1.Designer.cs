01de9cfae3afdcf27fd3a691f506341df8aec183
Ваулин Николай <nvvauin@gmail.com>
2015-05-07 00:33:54 +0400

    #DRN-136 добавил CMPMVS и  VisualSFM

--- CreateMap/Form1.Designer.cs
+++ CreateMap/Form1.Designer.cs
@@ -37,6 +37,9 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // progressBar1
@@ -56,7 +59,7 @@
             this.button1.ForeColor = System.Drawing.Color.Black;
             this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(209, 52);
+            this.button1.Size = new System.Drawing.Size(98, 52);
             this.button1.TabIndex = 1;
             this.button1.Text = "Запуск";
             this.button1.UseVisualStyleBackColor = false;
@@ -67,7 +70,7 @@
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(193)))), ((int)(((byte)(31)))));
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button2.Location = new System.Drawing.Point(227, 12);
+            this.button2.Location = new System.Drawing.Point(239, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(142, 21);
             this.button2.TabIndex = 2;
@@ -80,7 +83,7 @@
             this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(193)))), ((int)(((byte)(31)))));
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button3.Location = new System.Drawing.Point(227, 43);
+            this.button3.Location = new System.Drawing.Point(239, 43);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(142, 21);
             this.button3.TabIndex = 3;
@@ -99,34 +102,73 @@
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(193)))), ((int)(((byte)(31)))));
             this.label1.Location = new System.Drawing.Point(13, 71);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 4;
-            this.label1.Text = "Шаг : ";
             // 
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(226)))), ((int)(((byte)(150)))));
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.Location = new System.Drawing.Point(376, 12);
+            this.textBox1.Location = new System.Drawing.Point(387, 12);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(213, 20);
+            this.textBox1.Size = new System.Drawing.Size(202, 20);
             this.textBox1.TabIndex = 5;
             // 
             // textBox2
             // 
             this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(226)))), ((int)(((byte)(150)))));
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox2.Location = new System.Drawing.Point(375, 44);
+            this.textBox2.Location = new System.Drawing.Point(387, 44);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(213, 20);
+            this.textBox2.Size = new System.Drawing.Size(201, 20);
             this.textBox2.TabIndex = 6;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(193)))), ((int)(((byte)(31)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button4.ForeColor = System.Drawing.Color.Black;
+            this.button4.Location = new System.Drawing.Point(116, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(117, 52);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Очистить";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 147);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(575, 199);
+            this.listBox1.TabIndex = 8;
+            this.listBox1.Visible = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(193)))), ((int)(((byte)(31)))));
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button5.Location = new System.Drawing.Point(12, 120);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(577, 21);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Больше информаци";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(26)))), ((int)(((byte)(22)))));
-            this.ClientSize = new System.Drawing.Size(601, 126);
+            this.ClientSize = new System.Drawing.Size(601, 150);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
@@ -137,6 +179,8 @@
             this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.Name = "Form1";
             this.Text = "Создание 3d карты";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -152,6 +196,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- CreateMap/Form1.cs
+++ CreateMap/Form1.cs
@@ -7,20 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace CreateMap
 {
     public partial class Form1 : Form
     {
+        const int _SFM_PROC = 10;
+        const int _CMP_PROC = 11;
+        const int _SFM_PAUSE = 100;
+        const int _CMP_PAUSE = 101;
         string ImagePath;
         string LogPath;
         string outPath;
-        static string pathToVisualSFM = "visualSFM/visualSFM.exe";
-        static string pathToCMPMVS = "CMPMVS/cmpmvs.exe";
-        System.Diagnostics.ProcessStartInfo procSFM;
-        System.Diagnostics.ProcessStartInfo procCMPMVS;
-        bool runState = false;
-
+        static string pathToVisualSFM = @"\VisualSFM\VisualSFM.exe";
+        static string pathToCMPMVS = @"\CMPMVS.exe";
+        Process procSFM;
+        Process procCMPMVS;
+        bool pause = true;
+        int runState = 0;
         public Form1()
         {
             InitializeComponent();
@@ -28,36 +33,16 @@ namespace CreateMap
 
         private void button1_Click(object sender, EventArgs e)
         {
-            ImagePath = textBox1.Text ;
-            outPath = textBox2.Text;
-            if (!runState)
+            if (!(textBox1.Text == "" || textBox1.Text == ""))// || LogPath == null))
             {
-                if (!(outPath == null || ImagePath == null))// || LogPath == null))
-                {
-                    timer1.Enabled = true;
-                    runState = true;
-                    button1.Text = "Остановить";
-                    label1.Text = "Шаг 1 из 3 : нахождение позиций камер";
-                    //run create log to exif
-                    /*procSFM = new System.Diagnostics.ProcessStartInfo();
-                    procSFM.FileName = pathToVisualSFM; // Путь к приложению
-                    procSFM.UseShellExecute = false;
-                    procSFM.CreateNoWindow = true;
-                    procSFM.RedirectStandardOutput = true;
-                    procSFM.Arguments = "sfm+cmp " + ImagePath;
-                    System.Diagnostics.Process.Start(procSFM);*/
-                }
+                if(!pause)
+                    doPause();
                 else
-                {
-                    MessageBox.Show("Выберите путь к папке с изображениями и пусть для сохранения мкрты");
-                };
+                    doRun();
             }
             else
             {
-                timer1.Enabled = false;
-                runState = false;
-                button1.Text = "Запуск";
-                progressBar1.Value = 0;
+                MessageBox.Show("Выберите путь к папке с изображениями и пусть для сохранения мкрты");
             };
         }
 
@@ -78,7 +63,209 @@ namespace CreateMap
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            try
+            {
+                if (runState == 1)
+                {
+                    if (procSFM.HasExited == true)
+                        startCMPMVS();
+                    else
+                        doSFM();
+                };
+                if (runState == 2)
+                    if (procCMPMVS.HasExited == true)
+                    {
+                        turnToBegin();
+                        label1.Text = "all done";
+                    }
+                    else
+                        doCMPMVS();
+            }
+            catch (Exception e1)
+            {
+            };
             progressBar1.PerformStep(); ;
         }
+        void startSFM()
+        {
+            runState = 1;
+            label1.Text = "Шаг 1 из 3 : нахождение позиций камер";
+            procSFM = new Process();
+            procSFM.StartInfo.FileName = pathToVisualSFM;
+            procSFM.StartInfo.UseShellExecute = false;
+            procSFM.StartInfo.CreateNoWindow = true;
+            //procSFM.StartInfo.RedirectStandardOutput = true;
+            procSFM.StartInfo.Arguments = "sfm+cmp " + ImagePath + " " + outPath + @"\result";
+            procSFM.Start();
+        }
+
+        void startCMPMVS()
+        {
+            runState = 2;
+            label1.Text = "Шаг 2 из 3 : создание карты";
+            procCMPMVS = new Process();
+            procCMPMVS.StartInfo.FileName = pathToCMPMVS;
+            procCMPMVS.StartInfo.UseShellExecute = true;
+            procCMPMVS.StartInfo.CreateNoWindow = true;
+           // procCMPMVS.StartInfo.RedirectStandardOutput = true;
+            procCMPMVS.StartInfo.Arguments = outPath + @"\result.nvm.cmp\00\mvs.ini doComputeDEMandOrtoPhoto=TRUE ";
+            procCMPMVS.Start();
+        }
+
+        void doSFM()
+        {           
+            while (!procSFM.StandardOutput.EndOfStream) 
+            {
+                    string line = procSFM.StandardOutput.ReadLine();
+                    listBox1.Items.Add(line);
+            };
+        }
+
+        void doCMPMVS()
+        {           
+            while (!procCMPMVS.StandardOutput.EndOfStream) 
+            {
+                string line = procCMPMVS.StandardOutput.ReadLine();
+                listBox1.Items.Add(line);
+            };
+        }
+        void turnToBegin()
+        {
+            label1.Text = "";
+            listBox1.Items.Clear();
+            timer1.Enabled = false;
+            runState = 0;
+            button1.Text = "Запуск";
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Visible = false;
+            progressBar1.Value = 0;
+            try
+            {
+                procCMPMVS.Kill();                
+            }
+            catch (Exception e2)
+            {
+            }; 
+            try
+            {
+                procSFM.Kill();
+            }
+            catch (Exception e2)
+            {
+            };
+        }
+        void doPause()
+        {
+            pause = true;
+            timer1.Enabled = false;
+            button1.Text = "Запуск";
+            progressBar1.Value = 0;
+            try
+            {               
+                if(runState == 1)
+                {
+                    runState = 0;
+                    if(procSFM != null)
+                        if(!procSFM.HasExited)
+                            procSFM.Kill();
+                    turnToBegin();
+                    return ;
+                };
+            }
+            catch (Exception e)
+            {
+            };
+            try
+            {
+                if(runState == 2)
+                {
+                    runState = 1;
+                    if (procCMPMVS != null)
+                        if(!procCMPMVS.HasExited)
+                            procCMPMVS.Kill();
+                    return ;
+                };
+            }
+            catch (Exception e)
+            {
+            };
+        }
+        void doBegin()
+        {
+            button2.Enabled = false;
+            button3.Enabled = false;
+            ImagePath = textBox1.Text ;
+            outPath = textBox2.Text;
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            timer1.Enabled = true;
+            button4.Visible = true;
+        }
+
+        void doRun()
+        {            
+            button1.Text = "Пауза";
+            pause = false;
+            try
+            {
+                if (runState == 0)
+                {
+                    doBegin();
+                    startSFM();
+                    return;
+                };
+                if (runState == 1)
+                {
+                    startCMPMVS();
+                    return;
+                };
+            }
+            catch (Exception e)
+            {
+            };
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (button5.Text == "Меньше информаци")
+            {
+                button5.Text = "Больше информаци";
+                this.Height = 188;
+                listBox1.Visible = false;
+            }
+            else
+            {
+                button5.Text = "Меньше информаци";
+                if (this.Height < 400)
+                    this.Height = 400;
+                listBox1.Visible = true;
+            };
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            turnToBegin();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            turnToBegin();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string path = Application.ExecutablePath;
+            int i = path.Length-1;
+            while(path[i] != '\\')
+                --i;
+            path = path.Remove(i);
+            pathToVisualSFM = path + pathToVisualSFM;
+            pathToCMPMVS = path + pathToCMPMVS;
+        }
+        
     }
+
 }
