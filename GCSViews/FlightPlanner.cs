ac3d03df4f31773bd974038479bf8eb6515ada82
anatasiasia.f <anatastasia.f@gmail.com>
2015-05-23 16:05:53 +0300

    quickfix

--- GCSViews/FlightPlanner.cs
+++ GCSViews/FlightPlanner.cs
@@ -6125,7 +6125,8 @@ Column 1: Field type (RALLY is the only one at the moment -- may have RALLY_LAND
                     red.Clear();
                 if (green != null)
                     green.Clear();
-
+                if (red != null)
+                    red.Clear();
                 MainMap.Invalidate();
             }
         }
